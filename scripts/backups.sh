756477a11d3b17221651287bb7d15a49e7b14594
slugb0t <wheresdorian@gmail.com>
2025-06-11 22:49:53 -0700

    update

--- scripts/backups.sh
+++ scripts/backups.sh
@@ -16,7 +16,7 @@ set -o pipefail
 
  timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
 
-+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
 
  if [ -n "$PASSPHRASE" ]; then
    echo "Encrypting backup..."
